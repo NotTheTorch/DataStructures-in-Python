628a27dc4b48103d7237edb567ad25575ebb06e4
Vishnu Teja <112670215+NotTheTorch@users.noreply.github.com>
2024-06-17 13:06:46 +0530

    Update MoveZero.py

--- TwoPointers/MoveZero.py
+++ TwoPointers/MoveZero.py
@@ -1,4 +1,4 @@
-'''nums = [0,1,0,3,12]  # Output: [1,3,12,0,0]
+nums = [0,1,0,3,12]  # Output: [1,3,12,0,0]
 
 low = 0
 
@@ -8,31 +8,4 @@ for high in range(len(nums)):
         nums[low], nums[high] = nums[high],nums[low]
         low += 1
 
-print(nums)'''
-
-
-"""nums = [0,1,0,3,12]
-n = len(nums)
-high = n - 1
-
-
-for i in range(n):
-    if nums[i] == 0:
-        val = nums[i]
-        nums.remove(val)
-        nums.insert(high,val)
-
-print(nums)"""
-
-
-nums = [0,1,0,3,12]
-n = len(nums)
-res = [0] * n
-index = 0
-
-for i in range(n):
-    if nums[i] != 0:
-        res[index] = nums[i]
-        index += 1
-
-print(res)
+print(nums)
