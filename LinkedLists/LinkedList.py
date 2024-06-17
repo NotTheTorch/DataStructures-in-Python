779962b49412d4fb1ef93ebf77aad327015bdf18
Vishnu Teja <112670215+NotTheTorch@users.noreply.github.com>
2024-06-17 13:02:44 +0530

    Add files via upload

---
+++ LinkedLists/LinkedList.py
@@ -0,0 +1,114 @@
+class Node:
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_at_beginning(self,data):
+        node = Node(data,self.head)
+        self.head = node
+    
+    def insert_at_end(self,data):
+        if self.head is None:
+            self.head = Node(data,None)
+            return
+        
+        itr = self.head
+
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data,None)
+
+    def insert_values(self,data_set):
+        for data in data_set:
+            self.insert_at_end(data)
+
+    def getLength(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
+    
+    def remove_at(self,index):
+        if index < 0 and index > self.getLength():
+            raise Exception("Invalid Index")
+        if index == 0:
+            self.head = self.head.next
+            return
+        
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index-1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+
+    def insert_at(self,index,data):
+        if index < 0 and index > self.getLength():
+            raise Exception("Invalid Index")
+        if index == 0:
+            self.insert_at_beginning(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index-1:
+                node = Node(data,itr.next)
+                itr.next = node
+                break
+            
+            itr = itr.next
+            count += 1
+
+    def insert_value_after(self,data,data_to_insert):
+        itr = self.head
+        while itr:
+            if itr.data == data:
+                node = Node(data_to_insert,itr.next)
+                itr.next = node
+                return
+            itr = itr.next
+        raise ValueError(f"Element {data} not found in the list")
+    
+    def remove_by_value(self,data):
+        if self.head is None:
+            return 
+        if self.head.data == data:
+            self.head = self.head.next
+
+        itr = self.head
+        while itr and itr.next:
+            if itr.next.data == data:
+                itr.next = itr.next.next
+            itr = itr.next
+        return
+
+    def print(self):
+        lstr = ''
+        if self.head is None:
+            print("Linked List is empty")
+            return
+        
+        itr = self.head
+
+        while(itr):
+            lstr += str(itr.data) + "-->"
+            itr = itr.next
+        print(lstr)
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert_values(['apples','mangoes','grapes','orangese'])
+    ll.remove_at(2)
+    ll.insert_at(2,"figs")
+    ll.print()
+    print("Length of the linked list:",ll.getLength())
